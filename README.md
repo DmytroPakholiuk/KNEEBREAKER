aebdc99461ceca03cb3c55bd890f374ea90e1706
Dmytro Pakholiuk <dmytro0pakhoilluk@gmail.com>
2024-10-12 21:50:25 +0300

    added deployment through install.sh and docker-compose

--- README.md
+++ README.md
@@ -8,3 +8,15 @@ create an app that has "ACL" (Access Control List, a thing in Zend framework
 for authorization). The task specified it to have a configuration file for the ACL
 and have some sort of role inheritance, so I decided to code this in. It emulates the wat
 that Zend Framework may have it done, though RBAC is usually implemented otherwise in Laravel.
+I named it like that because the "ACL" also refers to some sort of knee injury.
+
+## Installation
+
+I have created an <code>install.sh</code> script that will do all the preparations for you.
+On Linux, just run it with the root permissions (it needs them to insert a record to your /etc/hosts file).
+<br>
+All subsequent starts of the project can be performed within the install.sh, or by running 
+<code>docker-compose up -d</code>
+<br>
+The site will be available at kneebreaker.com:20080 (don't forget
+to restart your browser after installation, before opening it: browsers cache the /etc/hosts file)
---
+++ install.sh
@@ -0,0 +1,162 @@
+#!/bin/bash
+
+success=0
+error=1
+
+OUT_COLOR_RED='\033[0;31m'
+OUT_COLOR_GREEN='\033[0;32m'
+OUT_COLOR_BLUE='\033[0;34m'
+OUT_NO_COLOR='\033[0m'
+
+# вывод цветного сообщения
+function output() {
+  case $2 in
+    success)
+      echo -e "$OUT_COLOR_GREEN$1$OUT_NO_COLOR"
+      ;;
+    error)
+      echo -e "$OUT_COLOR_RED$1$OUT_NO_COLOR"
+      ;;
+    * | info)
+      echo -e "$OUT_COLOR_BLUE$1$OUT_NO_COLOR"
+    ;;
+  esac
+}
+
+function yesno() {
+  default=''
+  if [[ ! (-z $2)  ]]; then
+   default=" [$2]"
+  fi
+  question="$1 (y/n)${default}:"
+  while true; do
+    read -p "${question}" answer
+    if [[ ${answer} = "" ]]; then
+        answer=$2
+    fi
+    case ${answer} in
+      Y | y | yes ) return ${success};;
+      N | n | no ) return ${error};;
+      * ) echo "Please answer yes or no.";;
+    esac
+  done
+}
+
+
+# запуск docker-compose
+function makeDocker() {
+
+  if ! (docker info); then
+    output "docker is not running. Can not continue" error
+    return ${error}
+  fi
+  if ! (hash docker-compose 2>/dev/null); then
+    output "docker-compose is not running. Can not continue." error
+    return ${error}
+  fi
+  if ! [[ -f 'docker-compose.yml' ]]; then
+    output "docker-compose.yml not found. Can not continue." error
+    return ${error}
+  fi
+  if ! (docker volume inspect kneebreaker_mysql_data >/dev/null); then
+    docker volume create --name=kneebreaker_mysql_data
+  fi
+
+  if ! (docker network inspect kneebreaker_network >/dev/null); then
+        docker network create --gateway 175.10.10.1 --subnet 175.10.10.0/24 kneebreaker_network
+  fi
+
+  if ! docker-compose up -d; then
+    output "docker-compose could not" error
+    return ${error}
+  fi
+
+  output "Docker containers have been set up successfully." success
+  return ${success}
+}
+
+function initialiseLaravel() {
+  output "updating laravel packages" info
+    docker exec kneebreaker_php bash -c "composer install && composer update"
+  output "updating laravel packages successful" success
+
+  output "generating api laravel encryption key" info
+    docker exec kneebreaker_php bash -c "php artisan key:generate"
+  output "key generation successful" success
+
+  output "running laravel migrations" info
+    docker exec kneebreaker_php bash -c "php artisan migrate"
+  output "laravel migrations successful" success
+
+
+  sudo chmod -R 777 storage/logs
+  sudo chmod -R 777 storage/framework
+
+
+}
+
+######################################
+function checkHosts() {
+    HOSTS='127.0.0.1 kneebreaker.com'
+    if grep "${HOSTS}" /etc/hosts | grep -v '^#'; then
+      echo "${HOSTS} уже присутствуют в /etc/hosts"
+    else
+      sudo /bin/bash -c "echo -e '\n${HOSTS}' >> /etc/hosts";
+      output "${HOSTS} have been added successfully to /etc/hosts." success
+    fi
+    output "The site is available at \n " info
+    output "kneebreaker.com:20080 " info
+}
+
+
+function fullInstall() {
+    copyEnv
+    if ! makeDocker; then
+        return
+    fi
+    initialiseLaravel
+    checkHosts
+}
+
+function start() {
+    docker-compose up -d
+}
+
+function copyEnv() {
+  if ! [[ -f '.env' ]]; then
+      cp .example.env .env
+  fi
+}
+
+function showInstallMenu() {
+  INSTALL='Full project installation'
+  START='Start the containers'
+
+  options=(
+      "${INSTALL}"
+      "${START}"
+  )
+
+    select opt in "${options[@]}"; do
+      case ${opt} in
+      ${INSTALL})
+        output "Full project installation" success
+        fullInstall
+        return
+        ;;
+      ${START})
+        start
+        return
+        ;;
+      *)
+    output 'Choose one of the shown options:' error
+    showInstallMenu
+    return
+      ;;
+    esac
+  done
+}
+
+showInstallMenu
+
+
